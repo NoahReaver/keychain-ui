810713c1601e81ad3dc202823a3f8975d70c5861
NoahReaver <emilianojaparicio@gmail.com>
2022-12-11 16:49:46 -0300

    Home Logo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,18 @@
 import ConnectWalletBtn from "./ConnectWalletBtn";
+import logo from "../assets/logos/Keychain Logo.png";
+
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <h2>Home</h2>
+      <div className={styles.logoContainer}>
+        <img src={logo} alt="logo" className={styles.logo} />
+        <p className={styles.logoText}>
+          Keychain is an on-chain component that combines your NFTs from
+          multiple wallets to be accessible by one account.
+        </p>
+      </div>
       <ConnectWalletBtn />
     </div>
   );
